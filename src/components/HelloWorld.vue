9dae3241b58b8436ae6ac2d9bc5d7d25a5a58c71
luozhou <zhou.luo1@chinacreator.com>
2020-04-28 15:32:37 +0800

    首页样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <div style="margin:0px 0px 20px 0px;width: 20%;padding: 0px 40%;">
+      <el-autocomplete
+        class="inline-input"
+        v-model="state1"
+        :fetch-suggestions="querySearch"
+        placeholder="请输入账号"
+        @select="handleSelect"
+      ></el-autocomplete>
+    </div>
+    <div style="margin:0px 0px 20px 0px;width: 20%;padding: 0px 40%;">
+      <el-input placeholder="请输入密码" v-model="input" show-password></el-input>
+    </div>
+    <div style="display:flex;padding: 0px 40%;">
+      <el-button type="primary" disabled>登录</el-button>
+       <el-button type="primary" disabled>注册</el-button>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  data() {
     return {
-      msg: '资源共享'
+      restaurants: [],
+      state1: "",
+      input: ""
+    };
+  },
+  methods: {
+    querySearch(queryString, cb) {
+      var restaurants = this.restaurants;
+      var results = queryString
+        ? restaurants.filter(this.createFilter(queryString))
+        : restaurants;
+      // 调用 callback 返回建议列表的数据
+      cb(results);
+    },
+    createFilter(queryString) {
+      return restaurant => {
+        return (
+          restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) ===
+          0
+        );
+      };
+    },
+    loadAll() {
+      return [
+        { value: "陈佳人", address: "长宁区新渔路144号" },
+        {
+          value: "柳基基",
+          address: "上海市长宁区淞虹路661号"
+        },
+        {
+          value: "死gaiba",
+          address: "上海市普陀区真北路988号创邑金沙谷6号楼113"
+        }
+      ];
+    },
+    handleSelect(item) {
+      console.log(item);
     }
+  },
+  mounted() {
+    this.restaurants = this.loadAll();
   }
-}
+};
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
